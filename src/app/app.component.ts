2322f3f05f395f1391514029d3eaa5f7d3b20c45
Macbook Pro Retina 13 <macbookproretina13@MacBook-Pro-de-Macbook.local>
2018-07-04 21:12:55 -0500

    [Update] (Main) first example for dragula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  
+  public tiles : Array<string> = ["flow","customer","agenda","promo","blog"];
+  public izquierda : Array<string> = ["flow", "agenda","blog"];
+  public derecha : Array<string> = ["customer", "promo"];
+  
+  ngOnInit(): void {
+    
+    // this.tiles.forEach((tile, index) => {
+    //   console.log(tile, index)
+    //   if(index % 2 == 0){
+    //     this.izquierda.push(tile)
+    //   }
+    //   else{
+    //     this.derecha.push(tile)
+    //   }
+    // });
+  }
+  
+  constructor(private dragulaService: DragulaService) {
+    dragulaService.dropModel.subscribe((value) => {
+      console.log(value)
+      this.onDropModel(value.slice(1));
+    });
+    dragulaService.removeModel.subscribe((value) => {
+      console.log(value)
+      this.onRemoveModel(value.slice(1));
+    });
+  }
+
+  private onDropModel(args) {
+    console.log(args)
+    let [el, target, source] = args;
+    // do something else
+  }
+
+  private onRemoveModel(args) {
+    console.log(args);
+    
+    let [el, source] = args;
+    // do something else
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DragulaModule } from 'ng2-dragula/ng2-dragula';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragulaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
